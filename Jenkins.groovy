3b1b7109942cfb5c968797a979006b8c2d194079
asinelnik <sinelnik.alek@yandex.ru>
2023-10-16 14:36:33 +0300

    Release

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,88 +1,81 @@
-task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-currentBuild.displayName = "$branch_cutted"
-base_git_url = "https://github.com/asinelnik/SwagLabs.git"
+pipeline {
+    agent any
+    tools {
+        maven 'maven'
+        jdk 'OpenJDK_8'
+    }
+    stages {
+        stage('Init') {
+            steps {
+                script {
+                    try {
+                        docker.image("aerokube/cm:latest-release")
+                                .withRun("--rm -v selenoid:/root/.aerokube/selenoid/ -v /var/run/docker.sock:/var/run/docker.sock",
+                                        "selenoid configure --browsers 'chrome' --last-versions 1 --tmpfs 128 --force") {}
+                    } catch (err) {
+                        echo err.getMessage()
+                    }
 
-
-node {
-    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
-        stage("Checkout Branch") {
-            if (!"$branch_cutted".contains("master")) {
-                try {
-                    getProject("$base_git_url", "$branch_cutted")
-                } catch (err) {
-                    echo "Failed get branch $branch_cutted"
-                    throw ("${err}")
+                    cleanWs()
+                    git credentialsId: 'gitlabUiCredentials', url: 'https://gitlab.mmtr.ru/naimixqa/naimixqa.git', branch: 'master'
                 }
-            } else {
-                echo "Current branch is master"
             }
         }
+        stage('Tests') {
+            steps {
+                script {
+                    try {
+                        docker.image("aerokube/selenoid:latest-release")
+                                .withRun("--rm -v /var/run/docker.sock:/var/run/docker.sock -v selenoid:/etc/selenoid/:ro --network=host", "--limit 12 --timeout 5m") {
+                                    container ->
+                                        withCredentials([
+                                                usernamePassword(credentialsId: 'siteAdmin', usernameVariable: 'siteAdminUser', passwordVariable: 'siteAdminPass'),
+                                                usernamePassword(credentialsId: 'dbUser', usernameVariable: 'dbUser', passwordVariable: 'dbPass'),
+                                                string(credentialsId: 'universalTestPassword', variable: 'universalTestPassword')
+                                        ]) {
+                                            withEnv([
+                                                    'PATH+EXTRA=/var/jenkins_home/tools/hudson.model.JDK/OpenJDK_8/jdk8u292-b05/bin',
+                                                    'JAVA_HOME=/var/jenkins_home/tools/hudson.model.JDK/OpenJDK_8/jdk8u292-b05'
+                                            ]) {
+                                                sh "mvn " +
+                                                        "-Dmaven.test.failure.ignore=true " +
+                                                        "-Dusers.webAdminSite.login=${siteAdminUser} " +
+                                                        "-Dusers.webAdminSite.password=${siteAdminPass} " +
+                                                        "-Dusers.db.login=${dbUser} " +
+                                                        "-Dusers.db.password=${dbPass} " +
+                                                        "-DdriverType=remote " +
+                                                        "-DselenoidUri=http://docker:4444/wd/hub " +
+                                                        "-Dusers.universalTestPassword=${universalTestPassword} " +
+                                                        "-f pom.xml " +
+                                                        "-DthreadCount=5 " +
+                                                        "-Dgroups=Checklist " +
+                                                        "-Dtestenv=${testenv} " +
+                                                        "test "
+                                                sh "docker logs --tail 500 ${container.id}"
+                                            }
+                                        }
 
-        try {
-            parallel getTestStages(["tests"])
-        } finally {
-            stage ("Allure") {
-                generateAllure()
+                                }
+                    } catch (err) {
+                        echo err.getMessage()
+                    }
+                }
             }
         }
-
-//        try {
-//            stage("Run tests") {
-//                parallel(
-//                        'Api Tests': {
-//                            runTestWithTag("apiTests")
-//                        },
-//                        'Ui Tests': {
-//                            runTestWithTag("uiTests")
-//                        }
-//                )
-//            }
-//        } finally {
-//            stage("Allure") {
-//                generateAllure()
-//            }
-//        }
     }
-}
-
-
-def getTestStages(testTags) {
-    def stages = [:]
-    testTags.each { tag ->
-        stages["${tag}"] = {
-            runTestWithTag(tag)
+    post {
+        always {
+            script {
+                allure([
+                        includeProperties: false,
+                        jdk: '',
+                        properties: [],
+                        reportBuildPolicy: 'ALWAYS',
+                        results: [
+                                [path: 'target/allure-results']
+                        ]
+                ])
+            }
         }
     }
-    return stages
 }
-
-
-def runTestWithTag(String tag) {
-    try {
-        labelledShell(label: "Run ${tag}", script: "chmod +x gradlew \n./gradlew -x test ${tag}")
-    } finally {
-        echo "some failed tests"
-    }
-}
-
-def getProject(String repo, String branch) {
-    cleanWs()
-    checkout scm: [
-            $class           : 'GitSCM', branches: [[name: branch]],
-            userRemoteConfigs: [[
-                                        url: repo
-                                ]]
-    ]
-}
-
-def generateAllure() {
-    allure([
-            includeProperties: true,
-            jdk              : '',
-            properties       : [],
-            reportBuildPolicy: 'ALWAYS',
-            results          : [[path: 'build/allure-results']]
-    ])
-}
-
